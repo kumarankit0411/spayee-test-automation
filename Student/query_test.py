e86de934dddb122ee4a162f531062927976dd0ba
Ankit Kr. Singh <ankitsingh095@outlook.com>
2017-06-12 23:47:43 +0530

    Added a test
    Query search test

---
+++ Student/query_test.py
@@ -0,0 +1,38 @@
+"""
+This script is written as a part of summer intern project 2017-18 for Spayee.
+Date: June 12, 2017 @ 11:34pm
+Author: Himadri Sharma
+Place: Noida, India
+Purpose: To automate query box search functionality testing for "https://learn.spayee.com/store"
+Test performed sequentially in order they are written
+Browser : Chrome
+"""
+
+from selenium import webdriver
+import unittest
+from selenium.webdriver.common.keys import Keys
+import os
+
+path = os.getcwd()
+path = path + "\chromedriver"
+
+
+class Query_box_test(unittest.TestCase):
+
+    @classmethod   
+    def setUpClass(self):
+        self.driver=webdriver.Chrome(path)
+        driver.get("https://learn.spayee.com/store")
+
+    def test_search_query(self):
+        driver = self.driver
+        elem = driver.find_element_by_name('query')
+        elem.send_keys("upsc books")
+        elem.send_keys(Keys.ENTER)
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
