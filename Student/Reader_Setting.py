7997c7fe4c61f135beee7ec3690c067b9dac27c0
Ankit Kr. Singh <ankitsingh095@outlook.com>
2017-07-12 21:51:08 +0530

    Improved Reader_setting test

--- Student/Reader_Setting.py
+++ Student/Reader_Setting.py
@@ -28,72 +28,73 @@ class ReadersSetting_check(unittest.TestCase) :
         self.driver.implicitly_wait(10)
 
     def test_readers_fonts(self):
-        elem=self.driver.find_element_by_id("readerSettingsBtn")
-        elem.click()
-        elem=self.driver.find_element_by_xpath('//*[@id="fontSizeChange"]/button[1]/i')
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_xpath('//*[@id="fontSizeChange"]/button[2]/i')
-        elem.click()
-        time.sleep(3)
-        print("test1 done")
+        self.driver.find_element_by_xpath('//*[@id="fontSizeChange"]/button[1]/i').click()
+        style = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("style")')
+        font_size = style[style.index('px')-2:style.index('px')]
+        assert float(font_size)==16
+
+        self.driver.find_element_by_xpath('//*[@id="fontSizeChange"]/button[2]/i').click()
+        style = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("style")')
+        font_size = style[style.index('px')-2:style.index('px')]
+        assert float(font_size)==18
 
     def test_line_height(self):
-        elem=self.driver.find_element_by_xpath('//*[@id="readerSettingsBtn"]')
-        elem.click()
-        elem=self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[1]')
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[2]')
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[3]')
-        elem.click()
-        time.sleep(3)
-        print("test2 done")
+        self.driver.find_element_by_xpath('//*[@id="readerSettingsBtn"]').click()
+        time.sleep(1)
+
+        '''small line height'''
+        self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[1]').click()
+        style = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("style")')
+        line_height = style[style.index('.')-1:style.index('.')+2]
+        assert float(line_height)==1.5 
+
+        '''medium line height'''
+        self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[2]').click()
+        style = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("style")')
+        line_height = style[style.index('.')-1:style.index('.')+2]
+        assert float(line_height)==2.5
+
+        '''large line height'''
+        self.driver.find_element_by_xpath('//*[@id="lineHeightChange"]/button[3]').click()
+        style = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("style")')
+        line_height = style[style.index('.')-1:style.index('.')+2]
+        assert float(line_height)==3.5
 
     def test_readers_justify(self):
-        elem=self.driver.find_element_by_xpath('//*[@id="readerSettingsBtn"]')
-        elem.click()
-        elem=self.driver.find_element_by_class_name("icon-paragraph-left")
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_class_name("icon-paragraph-center")
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_class_name('icon-paragraph-justify')
-        elem.click()
-        time.sleep(3)
-        print("test3 done")
+        '''Center alignment'''
+        self.driver.find_element_by_class_name("icon-paragraph-center").click()
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-center"
 
-    def test_annotations(self):
-        elem=self.driver.find_element_by_xpath('//*[@id="readerSettingsBtn"]')
-        elem.click()
-        elem=self.driver.find_element_by_xpath('//*[@id="annoMode"]/button[1]')
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_xpath('//*[@id="annoMode"]/button[2]')
-        elem.click()
-        time.sleep(3)
-        print("test4 done")
+        '''Justify alignment'''
+        self.driver.find_element_by_class_name("icon-paragraph-justify").click()
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-justify"
+
+        '''Left alignment'''
+        self.driver.find_element_by_class_name("icon-paragraph-left").click()
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-left"   
 
     def test_readers_theme(self):
-        elem=self.driver.find_element_by_xpath('//*[@id="readerSettingsBtn"]')
-        elem.click()
-        elem=self.driver.find_element_by_class_name('icon-sun')
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_class_name("icon-sun-2")
-        elem.click()
-        time.sleep(3)
-        elem=self.driver.find_element_by_class_name("icon-moon-2")
-        elem.click()
-        time.sleep(3)
-        print("test5 done")
+        '''Normal mode'''
+        self.driver.find_element_by_class_name('icon-sun').click
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-left"
+
+        '''Sepia mode'''
+        elem=self.driver.find_element_by_class_name("icon-sun-2").click()
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-left sepia"
+
+        '''Night mode'''
+        elem=self.driver.find_element_by_class_name("icon-moon-2").click()
+        class_name = self.driver.execute_script('return document.getElementById("pagingContainer").contentDocument.body.getAttribute("class")')
+        assert class_name=="spee-txt-align-left night"
 
     @classmethod
-    def tearDownClas(self):
-     self.driver.close()
+    def tearDownClass(self):
+        self.driver.quit()
 
 if __name__ ==  "__main__":
     unittest.main()
